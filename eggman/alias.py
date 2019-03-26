3a5a03055ab234cdb7f2a2c8532444293d3278f4
Niels Lindgren <niels.a.lindgren@gmail.com>
2019-03-26 11:16:12 -0700

    feat: Add full aliasing of starlette responses under eggman module
    Rather than having to import `starlette.responses` when defining the type annotations of your
    handler functions, we alias all possible starlette requests and responses in order to provide a
    simpler user experience.

--- eggman/alias.py
+++ eggman/alias.py
@@ -1,7 +1,15 @@
-from starlette.requests import Request as StarletteRequest
-from starlette.responses import Response as StarletteResponse
-from starlette.websockets import WebSocket as StarletteWebSocket
+import starlette.requests as starlette_requests
+import starlette.responses as starlette_responses
+import starlette.websockets as starlette_websockets
 
-Request = StarletteRequest
-Response = StarletteResponse
-WebSocket = StarletteWebSocket
+Request = starlette_requests.Request
+Response = starlette_responses.Response
+WebSocket = starlette_websockets.WebSocket
+
+HTMLResponse = starlette_responses.HTMLResponse
+PlainTextResponse = starlette_responses.Response
+JSONResponse = starlette_responses.JSONResponse
+UJSONResponse = starlette_responses.UJSONResponse
+RedirectResponse = starlette_responses.RedirectResponse
+StreamingResponse = starlette_responses.StreamingResponse
+FileResponse = starlette_responses.FileResponse
